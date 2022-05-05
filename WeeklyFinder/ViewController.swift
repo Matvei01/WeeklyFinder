c521be2626a7812692b6718af314c2a4c39268b8
Matvei95 <khlestov.95@mail.ru>
2022-05-05 18:47:35 +0500

    Working with code. Finish.

--- WeeklyFinder/ViewController.swift
+++ WeeklyFinder/ViewController.swift
@@ -8,6 +8,7 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
     @IBOutlet weak var dateTF: UITextField!
     @IBOutlet weak var monthTF: UITextField!
     @IBOutlet weak var yearTF: UITextField!
@@ -20,8 +21,32 @@ class ViewController: UIViewController {
         buttonFindDay.layer.cornerRadius = 10
    
     }
-
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        self.view.endEditing(true)
+        
+    }
+    
     @IBAction func findDay(_ sender: UIButton) {
+        
+        guard let day = dateTF.text, let month = monthTF.text, let year = yearTF.text else {return}
+        
+        let calendar = Calendar.current
+        var dateComponents = DateComponents()
+        dateComponents.day = Int(day)
+        dateComponents.month = Int(month)
+        dateComponents.year = Int(year)
+        
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "EEEE"
+        
+        guard let date = calendar.date(from: dateComponents) else {return}
+        
+        let weekday = dateFormatter.string(from: date)
+        
+        let capitalizedWeekday = weekday.capitalized
+        
+        resultLabel.text = capitalizedWeekday
         
     }
     
